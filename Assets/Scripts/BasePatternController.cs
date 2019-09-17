e1ea59703b39cfab8041e5b0f69947e02fc1a591
Umarji Krishna <umarjikrishna23@gmail.com>
2019-09-17 18:03:00 +0530

    Added new BasePatternController.cs Script which acts as base class for all the Patterns Moved the all Shape related algorithms to its own class so it becomes easy to maintain

---
+++ Assets/Scripts/BasePatternController.cs
@@ -0,0 +1,121 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasePatternController 
+{
+
+    protected GridIndex topElement = null, bottomElement = null, leftElement = null, rightElement = null;
+
+    protected void GetNeighbourElements(int currentActiveRow , int currentActiveCol)
+    {
+        topElement = bottomElement = leftElement = rightElement= null; 
+
+        if ((currentActiveRow - 1) >= GridManager.instance.minElements)
+            topElement = GridManager.instance.elementsList.Find(obj => obj.X == currentActiveRow - 1 && obj.Y == currentActiveCol);
+
+        if ((currentActiveRow + 1) < GridManager.instance.maxElements)
+            bottomElement = GridManager.instance.elementsList.Find(obj => obj.X == currentActiveRow + 1 && obj.Y == currentActiveCol);
+
+        if ((currentActiveCol - 1) >= GridManager.instance.minElements)
+            leftElement = GridManager.instance.elementsList.Find(obj => obj.X == currentActiveRow && obj.Y == currentActiveCol - 1);
+
+        if ((currentActiveCol + 1) < GridManager.instance.maxElements)
+            rightElement = GridManager.instance.elementsList.Find(obj => obj.X == currentActiveRow && obj.Y == currentActiveCol + 1);
+
+        #region Checking if elements are active
+        if (topElement != null && !topElement.isClicked)
+        {
+            topElement = null;
+        }
+
+        if (bottomElement != null && !bottomElement.isClicked)
+        {
+            bottomElement = null;
+        }
+
+        if (leftElement != null && !leftElement.isClicked)
+        {
+            leftElement = null;
+        }
+
+        if (rightElement != null && !rightElement.isClicked)
+        {
+            rightElement = null;
+        }
+            #endregion
+    }
+}
+
+public class SquarePattern : BasePatternController
+{
+    //Always the GridIndes is stored in following way 0 = Top , 1 = Bottom , 2 = Left , 3 = Bottom
+    private GridIndex[] gridIndices = null;
+
+    /// <summary>
+    /// Retuns all the Indices where the square is formed
+    /// </summary>
+    public void SquarePatternAlgorithm(List<GridIndex> activeElements)
+    {
+        for(int i = 0; i < activeElements.Count;i++)
+        {
+            int currentRow = activeElements[i].X;
+            int currentCol = activeElements[i].Y;
+
+            //Getting all the neighbors of the current element
+            GetNeighbourElements(currentRow , currentCol);
+
+            if (leftElement != null && bottomElement != null)
+            {
+                GridIndex bottomLeft = GridManager.instance.elementsList.Find(obj => obj.X == currentRow + 1 && obj.Y == currentCol - 1);
+
+                if (bottomLeft.isClicked)
+                {
+                    Debug.Log("Squared Formed at : " + currentRow + ":" + currentCol + "--" + leftElement.X + ":" + leftElement.Y + "--" +
+                                    bottomElement.X + ":" + bottomElement.Y + "--" + bottomLeft.X + ":" + bottomLeft.Y);
+                    GridManager.instance.text.SetActive(true);
+                    break;
+                }
+            }
+
+            else if (leftElement != null && topElement != null)
+            {
+                GridIndex topleft = GridManager.instance.elementsList.Find(obj => obj.X == currentRow - 1 && obj.Y == currentCol - 1);
+
+                if (topleft.isClicked)
+                {
+                    Debug.Log("Squared Formed at : " + currentRow + ":" + currentCol + "--" + leftElement.X + ":" + leftElement.Y + "--" +
+                                topElement.X + ":" + topElement.Y + "--" + topleft.X + ":" + topleft.Y);
+                    GridManager.instance.text.SetActive(true);
+                    break;
+                }
+            }
+
+            else if (rightElement != null && bottomElement != null)
+            {
+                GridIndex bottomRight = GridManager.instance.elementsList.Find(obj => obj.X == currentRow + 1 && obj.Y == currentCol + 1);
+
+                if (bottomRight.isClicked)
+                {
+                    Debug.Log("Squared Formed at : " + currentRow + ":" + currentCol + "--" + rightElement.X + ":" + rightElement.Y + "--" +
+                                 bottomElement.X + ":" + bottomElement.Y + "--" + bottomRight.X + ":" + bottomRight.Y);
+                    GridManager.instance.text.SetActive(true);
+                    break;
+                }
+            }
+
+            else if (rightElement != null && topElement != null)
+            {
+                GridIndex topRight = GridManager.instance.elementsList.Find(obj => obj.X == currentRow - 1 && obj.Y == currentCol + 1);
+
+                if (topRight.isClicked)
+                {
+                    Debug.Log("Squared Formed at : " + currentRow + ":" + currentCol + "--" + rightElement.X + ":" + rightElement.Y + "--" +
+                                 topElement.X + ":" + topElement.Y + "--" + topRight.X + ":" + topRight.Y);
+                    GridManager.instance.text.SetActive(true);
+                    break;
+                }
+            }
+        }
+    }
+}
--- Assets/Scripts/PatternDetector.cs
+++ Assets/Scripts/PatternDetector.cs
@@ -5,107 +5,22 @@ using UnityEngine.UI;
 
 public class PatternDetector : MonoBehaviour
 {
-    [HideInInspector]
     public List<GridIndex> activeElements = new List<GridIndex>();
+
 
     public void CheckForPattern(PATTERN_TYPE currentPattern)
     {
         switch(currentPattern)
         {
             case PATTERN_TYPE.SQUARE_FOUR_DOTS:
-            #region Square
 
-                for(int i = 0; i < activeElements.Count; i ++)
-                {
-                    int currentRow = activeElements[i].X;
-                    int currentCol = activeElements[i].Y;
+                SquarePattern squarePattern = new SquarePattern();
 
-                    GridIndex topElement = null , bottomElement = null, leftElement = null, rightElement = null;
+                squarePattern.SquarePatternAlgorithm(activeElements);
 
-                    #region Getting Top , Bottom , Left and Right Elements
-                    if ((currentRow - 1) >= GridManager.instance.minElements)
-                        topElement = GridManager.instance.elementsList.Find(obj => obj.X == currentRow - 1 && obj.Y == currentCol);
-
-                    if((currentRow + 1) < GridManager.instance.maxElements)
-                        bottomElement = GridManager.instance.elementsList.Find(obj => obj.X == currentRow + 1 && obj.Y == currentCol);
-
-                    if ((currentCol - 1) >= GridManager.instance.minElements)
-                        leftElement = GridManager.instance.elementsList.Find(obj => obj.X == currentRow && obj.Y == currentCol - 1);
-
-                    if ((currentCol + 1) < GridManager.instance.maxElements)
-                        rightElement = GridManager.instance.elementsList.Find(obj => obj.X == currentRow  && obj.Y == currentCol + 1);
-                    #endregion
-
-                    #region Checking if elements are active
-                    if (topElement != null && !topElement.isClicked)
-                        topElement = null;
-
-                    if (bottomElement != null && !bottomElement.isClicked)
-                        bottomElement = null;
-
-                    if (leftElement != null && !leftElement.isClicked)
-                        leftElement = null;
-
-                    if (rightElement != null && !rightElement.isClicked)
-                        rightElement = null;
-                    #endregion
-
-                    if (leftElement != null && bottomElement != null)
-                    {
-                        GridIndex bottomLeft = GridManager.instance.elementsList.Find(obj => obj.X == currentRow + 1 && obj.Y == currentCol - 1);
-
-                        if (bottomLeft.isClicked)
-                        {
-                            Debug.Log("Squared Formed at : " + currentRow + ":" + currentCol + "--" + leftElement.X + ":" + leftElement.Y + "--" +
-                                            bottomElement.X + ":" + bottomElement.Y + "--" + bottomLeft.X + ":" + bottomLeft.Y);
-                            GridManager.instance.text.SetActive(true);
-                            break;
-                        }
-                    }
-
-                    else if (leftElement != null && topElement != null)
-                    {
-                        GridIndex topleft = GridManager.instance.elementsList.Find(obj => obj.X == currentRow - 1 && obj.Y == currentCol - 1);
-
-                        if (topleft.isClicked)
-                        {
-                            Debug.Log("Squared Formed at : " + currentRow + ":" + currentCol + "--" + leftElement.X + ":" + leftElement.Y + "--" +
-                                        topElement.X + ":" + topElement.Y + "--" + topleft.X + ":" + topleft.Y);
-                            GridManager.instance.text.SetActive(true);
-                            break;
-                        }
-                    }
-
-                    else if (rightElement != null && bottomElement != null)
-                    {
-                        GridIndex bottomRight = GridManager.instance.elementsList.Find(obj => obj.X == currentRow + 1 && obj.Y == currentCol + 1);
-
-                        if (bottomRight.isClicked)
-                        {
-                            Debug.Log("Squared Formed at : " + currentRow + ":" + currentCol + "--" + rightElement.X + ":" + rightElement.Y + "--" +
-                                         bottomElement.X + ":" + bottomElement.Y + "--" + bottomRight.X + ":" + bottomRight.Y);
-                            GridManager.instance.text.SetActive(true);
-                            break;
-                        }
-                    }
-
-                    else if (rightElement != null && topElement != null)
-                    {
-                        GridIndex topRight = GridManager.instance.elementsList.Find(obj => obj.X == currentRow - 1 && obj.Y == currentCol + 1);
-
-                        if (topRight.isClicked)
-                        {
-                            Debug.Log("Squared Formed at : " + currentRow + ":" + currentCol + "--" + rightElement.X + ":" + rightElement.Y + "--" +
-                                         topElement.X + ":" + topElement.Y + "--" + topRight.X + ":" + topRight.Y);
-                            GridManager.instance.text.SetActive(true);
-                            break;
-                        }
-                    }
-                }
-            #endregion
                 break;
 
-            case PATTERN_TYPE.T_FIVE_DOTS:
+            /*case PATTERN_TYPE.T_FIVE_DOTS:
 
                 for (int i = 0; i < activeElements.Count; i++)
                 {
@@ -195,12 +110,12 @@ public class PatternDetector : MonoBehaviour
                     }
 
                 }
-                break;
+                break;*/
         }
     }
 
     public void ResetActiveElementsList()
     {
-        activeElements.Clear();
+        //activeElements.Clear();
     }
 }
